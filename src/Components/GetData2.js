e6a722b168ed64a8074a497f4ed946195125858f
Jatin Sonkusale <jatin22000@gmail.com>
2021-07-20 00:11:30 +0530

    added new api and custom input action

--- src/Components/GetData2.js
+++ src/Components/GetData2.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Axios from "axios";
+
+var Func = "trending";
 export default class Getdata2 extends React.Component {
   state = {
     results: [],
@@ -14,24 +16,44 @@ export default class Getdata2 extends React.Component {
   }
   render() {
     return (
-      <div className="main-display-hori">
-        {this.state.results.map((value, key) => (
-          <a
-            className="link"
-            key={key}
-            href={value.url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="newscard-hori">
-              <div className="imagep">
-                <img src={value.urlToImage} alt=""></img>
-                <span className="title-hori">{value.title}</span>
+      <span>
+        <input
+          onChange={(e) => {
+            Func = e.target.value;
+            console.log(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            Axios.get(
+              `https://bing-news-search1.p.rapidapi.com/news/search?q=${Func}&rapidapi-key=b388adc471msh65e8cd88ea66e29p1557f7jsnfa0be9110ed2`
+            ).then((res) => {
+              console.log(res.data.value);
+              this.setState({ results: res.data.value });
+            });
+          }}
+        >
+          Button
+        </button>
+        <div className="main-display-hori">
+          {this.state.results.map((value, key) => (
+            <a
+              className="link"
+              key={key}
+              href={value.url}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div className="newscard-hori">
+                <div className="imagep">
+                  <img src={value.urlToImage} alt=""></img>
+                  <span className="title-hori">{value.title}</span>
+                </div>
               </div>
-            </div>
-          </a>
-        ))}
-      </div>
+            </a>
+          ))}
+        </div>
+      </span>
     );
   }
 }
--- src/Components/Getdata.js
+++ src/Components/Getdata.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Axios from "axios";
 
+var Func = "trending";
 export default class Getdata extends React.Component {
   state = {
     results: [],
@@ -8,32 +9,59 @@ export default class Getdata extends React.Component {
 
   componentDidMount() {
     Axios.get(
-      `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=b5a02579e8674700a09deefa6f24af0c`
+      `https://bing-news-search1.p.rapidapi.com/news/search?q=${Func}&rapidapi-key=b388adc471msh65e8cd88ea66e29p1557f7jsnfa0be9110ed2`
     ).then((res) => {
-      this.setState({ results: res.data.articles });
+      this.setState({ results: res.data.value });
+      console.log(this.state.results);
     });
   }
   render() {
     return (
-      <div className="main-display">
-        {this.state.results.map((value, key) => (
-          <a key={key} href={value.url} target="_blank" rel="noreferrer">
-            <div className="newscard">
-              <div className="title">{value.title}</div>
-              <div className="author">
-                <span>Source: {value.source.name}</span>
-                <span className="date">{value.publishedAt}</span>
+      <span className="outer-div">
+        <input
+          onChange={(e) => {
+            Func = e.target.value;
+            console.log(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={() => {
+            Axios.get(
+              `https://bing-news-search1.p.rapidapi.com/news/search?q=${Func}&rapidapi-key=b388adc471msh65e8cd88ea66e29p1557f7jsnfa0be9110ed2`
+            ).then((res) => {
+              console.log(res.data.value);
+              this.setState({ results: res.data.value });
+            });
+          }}
+        >
+          Button
+        </button>
+        <div className="main-display">
+          {this.state.results.map((value, key) => (
+            <a key={key} href={value.url} target="_blank" rel="noreferrer">
+              <div className="newscard">
+                <div className="title">{value.name}</div>
+                <div className="author">
+                  <span>Source: {value.provider[0].name}</span>
+                  <span className="date">
+                    Published:
+                    {value.datePublished.toString().slice(0, 10)}
+                  </span>
+                </div>
+                <br></br>
+                <div className="imagep">
+                  <img
+                    src={value.image ? value.image.thumbnail.contentUrl : ""}
+                    alt="Noimagetodisplay"
+                  ></img>
+                </div>
+                <br></br>
+                <div className="summary">{value.content}</div>
               </div>
-              <br></br>
-              <div className="imagep">
-                <img src={value.urlToImage} alt=""></img>
-              </div>
-              <br></br>
-              <div className="summary">{value.content}</div>
-            </div>
-          </a>
-        ))}
-      </div>
+            </a>
+          ))}
+        </div>
+      </span>
     );
   }
 }
